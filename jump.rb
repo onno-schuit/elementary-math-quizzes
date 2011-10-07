f9c12cd147adb20675bdd8daf47a3c7e835bb0ee
Onno Schuit <o.schuit@solin.nl>
2011-10-07 20:13:44 +0200

    Completed jump exercise (with some presentation compromises)

--- jump.rb
+++ jump.rb
@@ -14,8 +14,9 @@ end
 
 def process_question(question)
   answer =  Array.new
+  print " - #{question} - \n"
   answer[0] = gets 
-  print " - #{question} - "
+  print "#{answer[0].chomp} - #{question} - \n"
   answer[1] = gets
   return answer
 end
@@ -28,11 +29,19 @@ def display_question
     question = create_small_jumps
   end
   answer = process_question(question)
-  if answer.chomp == 's' || answer.chomp == 'S'
+  if answer[0].chomp == 's' || answer[0].chomp == 'S' || answer[1].chomp == 's' || answer[1].chomp == 'S'
     puts "Goed: #{@@right}; fout: #{@@counter - @@right}. Totaal aantal sommen: #{@@counter}"
     return
   end
-
+  if answer[0].to_i == question - @@jump.to_i and  answer[1].to_i == question + @@jump.to_i 
+    puts "Goed!"
+    @@right += 1
+  else
+    puts "Nee, het juiste antwoord is: #{answer[0].to_i - @@jump.to_i} - #{question} - #{answer[1].to_i + @@jump.to_i}"
+  end
+  @@counter += 1
+  gets
+  display_question
 end
 
 
